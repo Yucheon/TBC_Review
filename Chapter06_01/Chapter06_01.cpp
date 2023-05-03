33aebb9b9a2b1a1fdadfe88e1f828dbe234af36f
Yucheon <103819552+Yucheon@users.noreply.github.com>
2023-05-04 05:41:11 +0900

    05/04 study

---
+++ Chapter06_01/Chapter06_01.cpp
@@ -0,0 +1,22 @@
+//배열 기초 [1 of 2] array
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int one_student_score;
+	int student_scores[5];
+
+	one_student_score = 100;
+
+	student_scores[0] = 100;
+	student_scores[1] = 56;
+	student_scores[2] = 87;
+	student_scores[3] = 80;
+	student_scores[4] = 90;
+
+	int my_array[5] = { 1,2,3,4,5 };
+
+	return 0;
+}
