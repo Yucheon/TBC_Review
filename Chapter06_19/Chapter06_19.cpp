a5445cb4f20517b6c3e31b961110adb54992063f
Yucheon <103819552+Yucheon@users.noreply.github.com>
2023-05-20 23:32:10 +0900

    05/20 study

--- Chapter06_19/Chapter06_19.cpp
+++ Chapter06_19/Chapter06_19.cpp
@@ -28,21 +28,27 @@ int main()
 		{11,12,13,14,15}
 	};
 
-	int* r1 = new int[col] {1, 2, 3, 4, 5};
-	int* r2 = new int [col] {6, 7, 8, 9, 10};
-	int* r3 = new int [col] {11, 12, 13, 14, 15};
+	int** matric = new int* [row];
 
-	int** rows = new int* [row] {r1, r2, r3};
+	for (int r = 0; r < row; ++r)
+		matric[r] = new int[col];
 
 	for (int r = 0; r < row; ++r)
 	{
 		for (int c = 0; c < col; ++c)
 		{
-			cout << rows[r][c] << " ";
+			matric[r][c] = s2da[r][c];
+		}
+	}
+
+	for (int r = 0; r < row; ++r)
+	{
+		for (int c = 0; c < col; ++c)
+		{
+			cout << matric[r][c] << " ";
 			cout << endl;
 		}
 	}
 
-	delete[]r1;
 	return 0;
 }
